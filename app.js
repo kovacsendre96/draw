f044d7c093e672c9d72e0a25a9a161745e1ee34b
Kovács Endre <kovacsendre96@gmail.com>
2020-11-20 14:42:51 +0100

    task7

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var context = canvas.getContext('2d');
 //context.fillStyle='black';
 //context.fillRect(canvasWidth/2,canvasHeight/2,canvasWidth/2,canvasHeight/2);
 
-let canvasWidth=canvas.width;//450
-let canvasHeight=canvas.height;//300
+//let canvasWidth=canvas.width;//450
+//let canvasHeight=canvas.height;//300
 //let padding=10;
 //let size=100;
 
@@ -27,20 +27,40 @@ let canvasHeight=canvas.height;//300
 //context.fillStyle = 'rgba(0,0,255,.5)';
 //context.fillRect(canvasWidth/2-size/4,canvasHeight/4+size/2,size,size);
 
-let sizeWidth=250;
-let sizeHeight=50;
+//let sizeWidth=250;
+//let sizeHeight=50;
 
-let w =canvasWidth/2-sizeWidth/2;
-let h =canvasHeight/2-sizeHeight/2;
-
-
-context.fillStyle = 'white';
-context.fillRect(w,h,sizeWidth,sizeHeight);
+//let w =canvasWidth/2-sizeWidth/2;
+//let h =canvasHeight/2-sizeHeight/2;
 
 
-context.fillStyle = 'red';
-context.fillRect(w,h-sizeHeight,sizeWidth,sizeHeight);
+//context.fillStyle = 'white';
+//context.fillRect(w,h,sizeWidth,sizeHeight);
 
 
-context.fillStyle = 'green';
-context.fillRect(w,h+sizeHeight,sizeWidth,sizeHeight);
+//context.fillStyle = 'red';
+//context.fillRect(w,h-sizeHeight,sizeWidth,sizeHeight);
+
+
+//context.fillStyle = 'green';
+//context.fillRect(w,h+sizeHeight,sizeWidth,sizeHeight);
+
+
+
+
+
+
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height;
+
+context.beginPath();
+context.moveTo(0,canvasHeight);
+context.lineTo(canvasWidth,0);
+context.strokeStyle = 'red';
+context.stroke();
+
+context.beginPath();
+context.moveTo(0,0);
+context.lineTo(canvasWidth,canvasHeight);
+context.strokeStyle = 'red';
+context.stroke();

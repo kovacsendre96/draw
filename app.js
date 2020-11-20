bc8d1d68f04259c75d66dd2fa989116bca432770
Kovács Endre <kovacsendre96@gmail.com>
2020-11-20 10:59:10 +0100

    task 2

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
-context.fillRect(175,100,100,100);
+
+
+context.fillStyle = '#D2691E';
+context.fillRect(125,50,200,200);

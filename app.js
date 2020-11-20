e481536b87aca910414236d27b839b0febcc9a3d
Kovács Endre <kovacsendre96@gmail.com>
2020-11-20 11:20:32 +0100

    task4

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var context = canvas.getContext('2d');
 
 //context.fillStyle = '#D2691E';
 //context.fillRect(125,50,200,200);
-context.beginPath();
-context.moveTo(0,300);
-context.lineTo(450,0);
-context.strokeStyle = 'red';
-context.stroke();
+//context.beginPath();
+//context.moveTo(0,300);
+//context.lineTo(450,0);
+//context.strokeStyle = 'red';
+//context.stroke();
 
+let canvasWidth=canvas.width;
+let canvasHeight=canvas.height;
+context.fillStyle='black';
+context.fillRect(canvasWidth/2,canvasHeight/2,canvasWidth/2,canvasHeight/2);

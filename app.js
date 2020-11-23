ca66baf4699d760752debbb35d4a523ef752598a
Kovács Endre <kovacsendre96@gmail.com>
2020-11-23 08:06:28 +0100

    task11

--- app.js
+++ app.js
@@ -84,14 +84,26 @@ let canvasHeight=canvas.height;
 // context.fillStyle = 'rgba(255,165,0,.5)';
 // context.fill();
 
-context.beginPath();
+/* context.beginPath();
 context.moveTo(canvasWidth/2,canvasHeight-canvasHeight);
 context.lineTo(canvasWidth/2,canvasHeight);
 context.strokeStyle = 'green';
-context.stroke();
+context.stroke(); */
 
-context.beginPath();
+/* context.beginPath();
 context.moveTo(canvasWidth-canvasWidth,canvasHeight/2);
 context.lineTo(canvasWidth,canvasHeight/2);
 context.strokeStyle = 'red';
-context.stroke();
+context.stroke(); */
+
+var padding = 20;
+var size = 45;
+var positionX = canvasWidth-canvasWidth+padding;
+var positionY = canvasHeight-canvasHeight+padding;
+
+for(let i = 0; i<9; i++){
+    context.fillStyle = "rgba(255,165,0,.5)";
+    context.fillRect(positionX, positionY, size, size);
+    positionX+=size/2;
+    positionY+=size/2;
+}

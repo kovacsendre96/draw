0eb2ddb88109622a4eefba7fb6320d101c56d6fc
Kovács Endre <kovacsendre96@gmail.com>
2020-11-24 14:16:35 +0100

    task23

--- app.js
+++ app.js
@@ -369,7 +369,7 @@ context.fillStyle = 'orange';
 
 
 //========TASK23========
-
+/*
 function drawCheckeredPattern(row,col){
     for(let j=0;j<row;j++){
 
@@ -410,4 +410,70 @@ if(j%2==1){
 }
 }
 
-drawCheckeredPattern(8,8);
+drawCheckeredPattern(8,8);*/
+
+
+//========TASK23========
+
+
+
+
+
+   
+    
+/*
+    context.beginPath();
+    context.moveTo(positionX,positionY);
+    context.lineTo(positionX+width/2,positionY+height);
+    context.lineTo(positionX-width/2,positionY+height);
+    context.lineTo(positionX,positionY);
+    context.strokeStyle ='black'
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(positionX+width/2,positionY+height);
+    context.lineTo(positionX,positionY+height*2);
+    context.lineTo(positionX+width,positionY+height*2);
+    context.lineTo(positionX+width/2,positionY+height);
+    context.strokeStyle ='black'
+    context.stroke();
+
+
+    context.beginPath();
+    context.moveTo(positionX-width/2,positionY+height);
+    context.lineTo(positionX,positionY+height*2);
+    context.lineTo(positionX-width,positionY+height*2);
+    context.lineTo(positionX-width/2,positionY+height);
+    context.strokeStyle ='black'
+    context.stroke();
+    
+*/
+
+var base = 100;
+var altitude = 86.6;
+
+function drawTriangle(positionX, positionY) {
+    for (var rowCounter = 0; rowCounter < 3; rowCounter++) {
+        for (var triangleCounter = 0; triangleCounter <= rowCounter; triangleCounter++) {
+            context.beginPath();
+            context.moveTo(positionX - rowCounter * base / 2 + triangleCounter * base, positionY + rowCounter * altitude);
+            context.lineTo(positionX - (rowCounter + 1) * base / 2 + triangleCounter * base, positionY + (rowCounter + 1) * altitude);
+            context.lineTo(positionX - (rowCounter - 1) * base / 2 + triangleCounter * base, positionY + (rowCounter + 1) * altitude);
+            context.lineTo(positionX - rowCounter * base / 2 + triangleCounter * base, positionY + rowCounter * altitude);
+            if (rowCounter === 0) {
+                context.strokeStyle = 'rgb(227,98,102)';
+                context.fillStyle = 'rgb(227,98,102)';
+            } else if (rowCounter === 1) {
+                context.strokeStyle = 'rgb(38,172,73)';
+                context.fillStyle = 'rgb(38,172,73)';
+            } else {
+                context.strokeStyle = 'rgb(34,128,128)';
+                context.fillStyle = 'rgb(34,128,128)';
+            }
+            context.stroke();
+            context.fill();
+        }
+    }
+}
+
+drawTriangle(225,33 );
